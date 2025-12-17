8060f1d68e4e589179bb0404ebdd6ff9cd926763
DOBBERMAN-COD <derrickmwangi145@gmail.com>
2025-12-17 12:15:10 +0300

    Add multiple platfroms

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -18,7 +18,24 @@ export default class Game extends Phaser.Scene {
 
     this.add.image(240, 320, "background");
 
-    //add a platfrom image in the middle
-    this.physics.add.image(240, 320, "platform").setScale(0.5);
+    // //add a platfrom image in the middle
+    // this.physics.add.image(240, 320, "platform").setScale(0.5);
+
+    //create the group
+    const platforms = this.physics.add.staticGroup();
+
+    //then create 5 platforms from the group
+    for (let i = 0; i < 5; ++i) {
+      const x = Phaser.Math.Between(80, 400);
+      const y = 150 * i;
+
+      /*@type{Phaser.Physics.arcade.Sprite} */
+      const platform = platforms.create(x, y, "platform");
+      platform.scale = 0.5;
+
+      /**@type{Phaser.Physics.Arcade.StaticBody} */
+      const body = platform.body;
+      body.updateFromGameObject(); // this will refresh tehe physics body based on any changes we made to the GameObject like position and scale
+    }
   }
 }
