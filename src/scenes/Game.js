da31197b5e8999fc016c7bf5ce41bb7dc57020b2
DOBBERMAN-COD <derrickmwangi145@gmail.com>
2025-12-17 11:21:46 +0300

    Detour into classes

---
+++ src/scenes/Game.js
@@ -0,0 +1,14 @@
+import Phaser from "../lib/phaser.js";
+
+export default class Game extends Phaser.Scene {
+  constructor() {
+    super("game"); //unique key defined by a Scene
+  }
+  preload() {
+    // called to allow us to specify images,audio or other assets to laod before starting the Scene
+  }
+
+  create() {
+    // called once all the assets for the Scene have been loaded.Only assets that have been loaded can be used in create()
+  }
+}
